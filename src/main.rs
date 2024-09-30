eb6d5f7d937c82a9e91446f1be6748d36d88b54d
Yelyzaveta Dymchenko <yelyzaveta.dymchenko@zippie.com>
2024-09-30 06:48:08 +0000

    fix: machine storing

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
 use advance_runner::run_advance;
 use cid::Cid;
 use clap::Parser;
-use futures_util::TryStreamExt;
+use futures::{StreamExt, TryStreamExt};
+use hyper::Request;
 use ipfs_api_backend_hyper::{IpfsApi, IpfsClient, TryFromUri};
+use rs_car_ipfs::single_file::read_single_file_seek;
 use std::collections::HashMap;
-use std::fs::File;
-use std::io::{Error, ErrorKind, Write};
-
+use std::io::{Error, ErrorKind};
 #[derive(Parser, Clone, Debug)]
 pub struct Options {
     #[clap(long, env = "IPFS_URL")]
@@ -23,30 +23,9 @@ pub struct Options {
 async fn main() {
     let opt = Options::parse();
     let cid = Cid::try_from(opt.cid).unwrap();
-    let ipfs_client = IpfsClient::from_str(&opt.ipfs_url).unwrap();
-    match ipfs_client
-        .get(&format!("{}/gov/app/info.json", cid.to_string()))
-        .map_ok(|chunk| chunk.to_vec())
-        .try_concat()
-        .await
-    {
-        Ok(res) => {
-            let mut file = match File::create("info.json") {
-                Ok(f) => f,
-                Err(e) => {
-                    eprintln!("error creating file: {}", e);
-                    return;
-                }
-            };
-
-            if let Err(e) = file.write_all(&res) {
-                eprintln!("error writing to file: {}", e);
-            }
-        }
-        Err(e) => eprintln!("error getting file: {}", e),
-    }
+    dedup_download_directory(&opt.ipfs_url, cid, "machine_source".to_string());
     run_advance(
-        String::from("info.json"),
+        String::from("machine_source"),
         "lambda_state_previous path",
         "lambda_state_next path",
         opt.payload,
@@ -64,4 +43,50 @@ fn report_callback(reason: u16, payload: &[u8]) -> Result<(u16, Vec<u8>), Error>
 
 fn output_callback(reason: u16, payload: &[u8]) -> Result<(u16, Vec<u8>), Error> {
     return Err(Error::from(ErrorKind::UnexpectedEof));
+}
+
+async fn dedup_download_directory(ipfs_url: &str, directory_cid: Cid, out_file_path: String) {
+    let ipfs_client = IpfsClient::from_str(ipfs_url).unwrap();
+    let res = ipfs_client
+        .ls(format!("/ipfs/{}", directory_cid.to_string()).as_str())
+        .await
+        .unwrap();
+
+    let first_object = res.objects.first().unwrap();
+
+    std::fs::create_dir_all(out_file_path.clone()).unwrap();
+
+    for val in &first_object.links {
+        let req = Request::builder()
+            .method("POST")
+            .uri(format!("{}/api/v0/dag/export?arg={}", ipfs_url, val.hash))
+            .body(hyper::Body::empty())
+            .unwrap();
+
+        let client = hyper::Client::new();
+
+        match client.request(req).await {
+            Ok(res) => {
+                let mut f = res
+                    .into_body()
+                    .map(|result| {
+                        result.map_err(|error| {
+                            std::io::Error::new(std::io::ErrorKind::Other, "Error!")
+                        })
+                    })
+                    .into_async_read();
+                let mut out = async_std::fs::OpenOptions::new()
+                    .read(true)
+                    .write(true)
+                    .create_new(true)
+                    .open(format!("{}/{}", out_file_path, val.name.clone()))
+                    .await
+                    .unwrap();
+                let root_cid = rs_car::Cid::try_from(val.hash.clone()).unwrap();
+
+                read_single_file_seek(&mut f, &mut out, None).await.unwrap();
+            }
+            Err(er) => {}
+        }
+    }
 }
